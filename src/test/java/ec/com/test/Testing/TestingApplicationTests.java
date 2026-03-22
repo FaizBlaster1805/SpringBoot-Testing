0f38e285a37a994d92bc07e2c0c0f139f7d8cd0c
Jhonars <jhon170304.jrs@gmail.com>
2026-03-22 13:39:50 -0500

    initial commit

---
+++ src/test/java/ec/com/test/Testing/TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.test.Testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
